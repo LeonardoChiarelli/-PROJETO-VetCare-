243b4238e201b75f25b3d6b1b29e6b9f88d02941
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-01-29 22:31:29 -0300

    Modificações na parte de Usuários

--- vetCareAPI/src/main/java/br/com/LeoChiarelli/vetCareAPI/domain/Veterinario/controller/UsuarioController.java
+++ vetCareAPI/src/main/java/br/com/LeoChiarelli/vetCareAPI/domain/Veterinario/controller/UsuarioController.java
@@ -3,11 +3,17 @@ package br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.controller;
 import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.dto.CadastrarUsuarioDTO;
 import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.dto.EfetuarLoginDTO;
 import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.dto.TokenJWTDTO;
+import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.model.Usuario;
+import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.repository.IUsuarioRepository;
 import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.service.UsuarioService;
+import br.com.LeoChiarelli.vetCareAPI.general.security.TokenService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,17 +26,33 @@ public class UsuarioController {
     @Autowired
     private UsuarioService service;
 
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private IUsuarioRepository repository;
+
+
     @PostMapping("/cadastrar")
     @Transactional
-    public ResponseEntity<String> cadastrarUsuario(@RequestBody @Valid CadastrarUsuarioDTO dto){
-        service.cadastrar(dto);
+    public void cadastrar(@RequestBody @Valid CadastrarUsuarioDTO dto) {
+        String senhaEncriptada = passwordEncoder.encode(dto.senha());
 
-        return ResponseEntity.ok().body("Usuário criado com sucesso!");
+        repository.save(new Usuario(dto.nome(), dto.email(), senhaEncriptada));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> efetuarLogin(@RequestBody @Valid EfetuarLoginDTO dto){
-        var tokenJWT = service.efetuarLogin(dto);
+    public ResponseEntity<String> efetuarLogin(@RequestBody @Valid EfetuarLoginDTO dto) {
+        var authenticationManager = new UsernamePasswordAuthenticationToken(dto.email(), dto.senha());
+        var authentication = manager.authenticate(authenticationManager);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
         return ResponseEntity.ok(tokenJWT);
     }
--- vetCareAPI/src/main/java/br/com/LeoChiarelli/vetCareAPI/domain/Veterinario/service/UsuarioService.java
+++ vetCareAPI/src/main/java/br/com/LeoChiarelli/vetCareAPI/domain/Veterinario/service/UsuarioService.java
@@ -1,53 +1,17 @@
 package br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.service;
 
-import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.dto.CadastrarUsuarioDTO;
-import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.dto.EfetuarLoginDTO;
-import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.dto.TokenJWTDTO;
-import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.model.Usuario;
 import br.com.LeoChiarelli.vetCareAPI.domain.Veterinario.repository.IUsuarioRepository;
-import br.com.LeoChiarelli.vetCareAPI.general.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService implements UserDetailsService {
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private AuthenticationManager manager;
-
-    @Autowired
-    private TokenService tokenService;
-
-    @Autowired
     private IUsuarioRepository repository;
-
-    public void cadastrar(CadastrarUsuarioDTO dto) {
-        String senhaEncriptada = passwordEncoder.encode(dto.senha());
-
-        repository.save(new Usuario(dto.nome(), dto.email(), senhaEncriptada));
-    }
-
-    public ResponseEntity<?> efetuarLogin(EfetuarLoginDTO dto) {
-        try {
-            var authenticationManager = new UsernamePasswordAuthenticationToken(dto.email(), dto.senha());
-            var authentication = manager.authenticate(authenticationManager);
-
-            var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
-            return ResponseEntity.ok(new TokenJWTDTO(tokenJWT));
-        } catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
