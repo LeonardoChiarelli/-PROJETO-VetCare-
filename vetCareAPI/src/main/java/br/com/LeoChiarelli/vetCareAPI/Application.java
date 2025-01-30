83103db40110c7c2956cfb838daf189723bd971d
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-01-29 21:37:40 -0300

    Update Application.java

--- vetCareAPI/src/main/java/br/com/LeoChiarelli/vetCareAPI/Application.java
+++ vetCareAPI/src/main/java/br/com/LeoChiarelli/vetCareAPI/Application.java
@@ -3,7 +3,7 @@ package br.com.LeoChiarelli.vetCareAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.LeoChiarelli.vetCareAPI")
 public class Application {
 
 	public static void main(String[] args) {
