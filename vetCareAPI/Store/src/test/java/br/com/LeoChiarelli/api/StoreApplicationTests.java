cfc356157e2bb29fdcdcbe97a4784a4f049adc31
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-02-06 20:20:05 -0300

    Criação do módulo Store

---
+++ vetCareAPI/Store/src/test/java/br/com/LeoChiarelli/api/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.LeoChiarelli.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
