0cabeb22b24b41e150ccc7d00fbcff5f0077302b
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-02-23 15:11:23 -0300

    Update IPerfilRepository.java

--- vetCareAPI/Adocao/src/main/java/br/com/LeoChiarelli/api/domain/repository/IPerfilRepository.java
+++ vetCareAPI/Adocao/src/main/java/br/com/LeoChiarelli/api/domain/repository/IPerfilRepository.java
@@ -3,5 +3,8 @@ package br.com.LeoChiarelli.api.domain.repository;
 import br.com.LeoChiarelli.api.domain.model.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IPerfilRepository extends JpaRepository<Perfil, Long> {
+    Optional<Perfil> findByNome(String nome);
 }
