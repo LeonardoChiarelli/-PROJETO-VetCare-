8a302b14bb29f6c397e2f6311d020a34081740b8
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-02-26 15:46:20 -0300

    Update TutorController.java

--- vetCareAPI/Adocao/src/main/java/br/com/LeoChiarelli/api/domain/controller/TutorController.java
+++ vetCareAPI/Adocao/src/main/java/br/com/LeoChiarelli/api/domain/controller/TutorController.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
