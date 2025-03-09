028d3807b7c1c4a8675b667ccd8b63b70a469648
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-03-09 17:49:46 -0300

    Case Perfil / Usuário

---
+++ vetCareAPI/Store/src/main/java/br/com/LeoChiarelli/api/domain/model/Perfil.java
@@ -0,0 +1,40 @@
+package br.com.LeoChiarelli.api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "perfis")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+public class Perfil implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public boolean isAdmin(){
+        return this.nome.equals("ROLE_ADMIN");
+    }
+
+    public boolean isComprador(){
+        return this.nome.equals("ROLE_COMPRADOR");
+    }
+
+    public boolean isVisualizador(){
+        return this.nome.equals("ROLE_VISUALIZADOR");
+    }
+
+    @Override
+    public String getAuthority() {
+        return nome;
+    }
+}
