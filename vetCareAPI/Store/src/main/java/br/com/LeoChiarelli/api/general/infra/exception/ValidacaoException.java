c831424ed49e8da46696b4435cb8983b7f7466b3
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-03-09 17:49:50 -0300

    Create ValidacaoException.java

---
+++ vetCareAPI/Store/src/main/java/br/com/LeoChiarelli/api/general/infra/exception/ValidacaoException.java
@@ -0,0 +1,5 @@
+package br.com.LeoChiarelli.api.general.infra.exception;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String message) {super(message);}
+}
