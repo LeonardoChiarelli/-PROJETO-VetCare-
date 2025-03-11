bd46c83b5fcfc26d8153c1c5e667705446da9697
Leonardo Chiarelli <devleochiarelli@gmail.com>
2025-03-11 19:15:30 -0300

    Update IPedidoRepository.java

--- vetCareAPI/Store/src/main/java/br/com/LeoChiarelli/api/domain/repository/IPedidoRepository.java
+++ vetCareAPI/Store/src/main/java/br/com/LeoChiarelli/api/domain/repository/IPedidoRepository.java
@@ -1,7 +1,30 @@
 package br.com.LeoChiarelli.api.domain.repository;
 
+import br.com.LeoChiarelli.api.domain.dto.EstatisticasVendasDTO;
 import br.com.LeoChiarelli.api.domain.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface IPedidoRepository extends JpaRepository<Pedido, Long> {
+
+    @Query("SELECT SUM(i.precoUnitario * i.quantidade) FROM Pedido p JOIN p.itens i WHERE p.data = :dataOntem")
+    BigDecimal faturamentoTotalDoDia(LocalDate dataOntem);
+
+    @Query("""
+            SELECT NEW br.com.LeoChiarelli.api.domain.dto.EstatisticasVendasDTO(
+                prod.cataegoria,
+                SUM(i.quantidade),
+                SUM(i.precoUnitario * i.quantidade)
+            )
+            FROM Pedido p
+            JOIN p.itens i
+            JOIN i.produto prod
+            WHERE p.data = :dataOntem
+            GROUP BY prod.catagoria
+            """)
+    List<EstatisticasVendasDTO> faturamentoTotalPorCategoriaEDia(LocalDate dataOntem);
 }
